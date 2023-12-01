c4a122e384077f14c5c34064836d7c4e3672c727
SantiSanchezMar <ssanchezmarostica@itba.edu.ar>
2023-12-01 18:02:58 -0300

    primera version de toMatrix
    por medio de un arreglo de chars podemos aprovechar para pasar a matriz y flitrar la informacion del archivo, guardando la importante y retorna la matriz filtrada
    
    Co-Authored-By: manuothatceguy <141188032+manuothatceguy@users.noreply.github.com>
    Co-Authored-By: gregoriotiscornia <146454480+gregoriotiscornia@users.noreply.github.com>

--- csvTools/csvTools.c
+++ csvTools/csvTools.c
@@ -2,7 +2,7 @@
 #include <errno.h>
 #include <stdlib.h>
 
-#define MAXCHAR 1000
+#define MAXCHAR 100
 #define BLOCK 10
 
 static int checkErrno(void * ptr){
@@ -13,47 +13,75 @@ static int checkErrno(void * ptr){
     return 0;
 }
                             
-char *** toMatrix(FILE * fp, int * colNbr){
+char *** toMatrix(FILE * fp, unsigned int colNbr, char colsToFilter[], unsigned int * rowsNew){ // colNbr es la dimension de colsToFilter
 
     char row[MAXCHAR];
     char *token;
-    int isFirstRow = 1;
-    char *** retMatrix = NULL;
-    int dim;
-    int copy = 0;
-
+    int isFirstRow = 1; //Flag para no copiar la primera fila con los títulos
+    char*** retMatrix = NULL;
+    int len = 0;    // Longitud del token copiado
+    int copy = 0;   // Cantidad de filas de la matriz destino
+    int col = 0;    // Indice de la columna de la matriz fuente.
+    int newColDim = 0;  // Indice de la columna de la matriz destino.
+    for(int i = 0; i < colNbr; i++){
+        if(colsToFilter[i])
+            newColDim++;
+    }
+    int newColIdx;
     while (!feof(fp))
-    {
-        
+    {   
+        col = 0;
+        newColIdx = 0;
         fgets(row, MAXCHAR, fp);
-        int col = 0;
+
         token = strtok(row, ";");
+        int valid;
 
         while(token != NULL)
         {
+            valid = (colsToFilter[col])==1;
             if(isFirstRow != 1){
                 if(copy % BLOCK == 0){
                     errno = 0;
                     retMatrix = realloc(retMatrix,(copy+BLOCK)*sizeof(char**));
                     if(checkErrno((void*)retMatrix))
                         return NULL;
-                }
-                
-                dim = 0;
-
-                while(*token != '\0' && *token != '\n'){
-                    if(dim % BLOCK == 0 ){
+                    for(int i = copy; i < copy+BLOCK; i++){
                         errno = 0;
-                        retMatrix[copy][col] = realloc(retMatrix[copy][col],(dim + BLOCK));
-                        if(checkErrno((void*)retMatrix[copy][col]))
+                        retMatrix[i] = realloc(retMatrix[i],(newColDim)*sizeof(char*));
+                        if(checkErrno(retMatrix[i])){
                             return NULL;
+                        }
                     }
                 }
+
+                len = 0;
+
+                if(valid){
+                    while(*token != '\0' && *token != '\n'){
+                        if(len % BLOCK == 0){
+                        errno = 0;
+                        retMatrix[copy][newColIdx] = realloc(retMatrix[copy][newColIdx],(len + BLOCK));
+                        if(checkErrno((void*)retMatrix[copy][newColIdx]))
+                            return NULL;
+                        }
+                        retMatrix[copy][newColIdx][len++] = *token;
+                        token++;
+                    }
+                    errno=0;
+                    retMatrix[copy][newColIdx] = realloc(retMatrix[copy][newColIdx],(len)*sizeof(char));
+                    if(checkErrno((void*)retMatrix[copy][newColIdx])){
+                        return NULL;
+                    }
+                    retMatrix[copy][newColIdx++][len] = '\0';
+                }
+                col++;
             }
             token = strtok(NULL, ";");
         }
         isFirstRow = 0; // apago flag
         
     }
-
+    *rowsNew = copy;    // Así se sabe hasta donde acceder, ya que la matriz no tiene marca de fin.
+    return retMatrix;
 }
