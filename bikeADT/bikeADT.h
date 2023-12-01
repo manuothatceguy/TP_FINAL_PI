e6b58c597b456defee017e2daec5ec1d75e26e23
SantiSanchezMar <ssanchezmarostica@itba.edu.ar>
2023-12-01 12:39:41 -0300

    Update bikeADT.h
    Co-Authored-By: manuothatceguy <141188032+manuothatceguy@users.noreply.github.com>

--- bikeADT/bikeADT.h
+++ bikeADT/bikeADT.h
@@ -3,7 +3,33 @@
  * PARA PROCESAR DATOS DE ALQUILERES DE BICICLETAS
 */
 
+#include <stdio.h>
+
 #ifndef BIKE_ADT_H
 #define BIKE_ADT_H
+
+typedef struct bikeCDT * bikeADT;
+
+/**
+ * @param -
+ * @return Un ADT vacío que espera llenarse
+ * @brief Esta función debe ejecutarse al iniciar con este ADT.
+*/
+bikeADT newBikeADT(void);
+
+/**
+ * @param bikes el ADT previamente inicializado con la función newBikeADT
+ * @param stationFrom ID de la estación de origen del viaje
+ * @param stationTo ID de la estación de destino del viaje
+ * @param startDate fecha y hora en la que se inició el viaje
+ * @param endDate fecha y hora en la que se finalizó el viaje
+ * @param isMember si el usuario es miembro del sistema o no.
+ * @return 1 si se agregó, 0 si no se agregó
+ * @brief Agrega un viaje, si alguna de las estaciones no existe las agrega.
+ * 
+*/
+int addTrip(bikeADT bikes, unsigned int stationFrom, unsigned int stationTo, char * startDate, char * endDate, char isMember);
+
+
 
 #endif
