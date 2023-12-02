294490ad7fe46cd3a09b0fc0e4c1fafc8e2ee7e4
manuothatceguy <mothatceguy@itba.edu.ar>
2023-12-02 10:18:21 -0300

    Update frontMON.c
    mejora front y agrega primer ingreso de datos al ADT. Enum para mayor claridad al usar las referencias a la tabla resultado de filtrar en el CSV
    
    Co-Authored-By: gregoriotiscornia <146454480+gregoriotiscornia@users.noreply.github.com>
    Co-Authored-By: SantiSanchezMar <152513671+SantiSanchezMar@users.noreply.github.com>

--- frontMON.c
+++ frontMON.c
@@ -4,6 +4,9 @@
 #include "csvTools/csvTools.h"    // tambien revisar estiloo!!!!!!!!!!!!!!!!
 
 #define CANT_COLS_STATIONS_CSV 4
+#define CANT_COLS_BIKESMON_CSV 5
+
+enum posInCsv {START_DATE = 0, ID_START, END_DATE, ID_END, MEMBER_STATUS};
 
 int main(int argc, char const *argv[])
 {
@@ -21,16 +24,20 @@ int main(int argc, char const *argv[])
          exit(2);
         }
         
-        char ** row;
-        int stationCount = 0;
-        elemVec * stations;
+        
+        
+        
 
-        char filtro[] = {1,1,0,0}; // Porque solamente se necesitan las columnas del nombre y del id
-
-        int fils = 0;
-        char *** filter = toMatrix(stations,CANT_COLS_STATIONS_CSV,filtro,&fils);
-
-        elemVec stationNameId[fils];
+        char filtroStations[] = {1,1,0,0}; // Porque solamente se necesitan las columnas del nombre y del id
+        char filtroTrips[] = {1,1,1,1,1}; // Porque necesito todas las columnas, en NYC no necesitaría el tipo de bicicleta, por ejemplo.
+        int stationsNum = 0;
+        int tripNum = 0;
+        char *** stationsFilter = toMatrix(stations,CANT_COLS_STATIONS_CSV,filtroStations,&stationsNum);
+        char *** tripsFilter = toMatrix(bikes,5,filtroTrips,&tripNum);
+        bikeADT bikesMon = newBikeADT(stationsFilter,stationsNum,0);
+        for(int i = 0; i < tripNum; i++){
+            addTrip(bikesMon,atoi(tripsFilter[i][ID_START]),atoi(tripsFilter[i][ID_END]),tripsFilter[i][START_DATE],tripsFilter[i][END_DATE],*tripsFilter[i][MEMBER_STATUS]);
+        }
 
         
 
